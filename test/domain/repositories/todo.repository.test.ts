737dfe4dc42685e0b89c5eb1bbeba1cb34a053b2
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-11-08 15:35:21 +0100

    testing TodoRepository

---
+++ test/domain/repositories/todo.repository.test.ts
@@ -0,0 +1,62 @@
+import { TodoRepository } from "../../../src/domain";
+import { TodoEntity } from "../../../src/domain";
+import { CreateTodoDto, UpdateTodoDto } from "../../../src/domain";
+
+describe('todo.repository.ts: testing TodoRepository', ()=> {
+
+    const newTodo =  new TodoEntity(1,'testing todo-repository')
+
+   
+    class MockTodoDatasource implements TodoRepository{
+
+        async getAll(): Promise<TodoEntity[]>{
+            return [newTodo];
+         }
+
+        async create(createTodoDto: CreateTodoDto): Promise<TodoEntity>{
+           return newTodo;
+        }
+    
+        async findById(id: number): Promise<TodoEntity>{
+            return newTodo;
+        }
+    
+         async updateById(updateTodoDto: UpdateTodoDto): Promise<TodoEntity>{
+           return newTodo
+         }
+    
+         async deleteById(id: number): Promise<TodoEntity>{
+            return newTodo;
+         }
+    }
+
+
+    test('should test the abstract class', async()=>{
+        const mockTodoDatasource = new MockTodoDatasource();
+
+        expect(mockTodoDatasource).toBeInstanceOf(MockTodoDatasource);
+        // expect(mockTodoDatasource).toHaveProperty('getAll');
+        // expect(mockTodoDatasource).toHaveProperty('create');
+        // expect(mockTodoDatasource).toHaveProperty('findById');
+        // expect(mockTodoDatasource).toHaveProperty('updateById');
+        // expect(mockTodoDatasource).toHaveProperty('deleteById');
+
+        expect(typeof mockTodoDatasource.getAll).toBe('function');
+        expect(typeof mockTodoDatasource.create).toBe('function');
+        expect(typeof mockTodoDatasource.findById).toBe('function');
+        expect(typeof mockTodoDatasource.updateById).toBe('function');
+        expect(typeof mockTodoDatasource.deleteById).toBe('function');
+
+        const todos = await mockTodoDatasource.getAll();
+       const createdTodo = await mockTodoDatasource.create(newTodo);
+       const foundTodo = await mockTodoDatasource.findById(newTodo.id);
+       const deletedTodo =await mockTodoDatasource.deleteById(newTodo.id)
+        //await mockTodoDatasource.updateById(newTodo.id.)
+        expect(todos).toHaveLength(1);
+        expect(createdTodo).toBeInstanceOf(TodoEntity);
+        expect(foundTodo).toBeInstanceOf(TodoEntity);
+        expect(deletedTodo).toBeInstanceOf(TodoEntity);
+
+
+    })
+})
